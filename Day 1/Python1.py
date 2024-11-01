cc6fec84f9598ef9de2d4fea1c97d4ef4767e54e
Aryan-panda <aryan06panda@gmail.com>
2024-11-01 09:35:18 +0530

    Update Python1.py

--- Day 1/Python1.py
+++ Day 1/Python1.py
@@ -1,1 +1,60 @@
-print ("Hello")
+#First Code (Warm up)
+"""
+print ("Hello\n")
+Name = input ( "Enter your Name : " )
+Surname = input ( "Enter your Surname : " )
+if bool(Name) or bool(Surname) == True :
+    Full_Name = Name+" "+Surname
+    print (f"So, you are {Full_Name}.")
+    print ("\nUser : You are goddamn right.\n")
+else :
+    print ("No Input. ")
+"""
+# ------------------------------------------------------------------------------------------------------------------------
+#Second Code (Adult madlibs game🔞)
+"""
+Your_Fiend_First_Name = str ( input ("Enter your friend's first name : "))
+Your_Friend_Last_Name = str (input ("Enter your friend's last name : "))
+Your_Name = str ( input ("Enter your name : "))
+Activity = str ( input ("Enter an activity : "))
+Body_part = str ( input ("Enter one of your body part : "))
+Item = str ( input ("Enter an item of clothing : "))
+Place = str ( input ("Enter a place of house : "))
+Object = str ( input ("Enter an insertable object : "))
+Orifice = str ( input ("Enter an orifice on your friend : "))
+Celebrity = str ( input ("Enter a celebrity name : "))
+Exclamation = str ( input ("Enter an exclamation of frustration : "))
+
+if bool(Your_Fiend_First_Name) and bool(Your_Friend_Last_Name) and bool(Your_Name) and bool(Activity) and bool(Body_part) and bool(Item) and bool(Place) and bool(Object) and bool(Orifice) and bool(Celebrity) and bool(Exclamation) :
+    print (f"You answer your mobile phone and you are pleasantly surprised to hear {Your_Fiend_First_Name} {Your_Friend_Last_Name}’s voice.")
+    print (f"“Hi {Your_Name},” says {Your_Fiend_First_Name}. “What are you doing?”")
+    print (f"“I’m {Activity},” you reply. “But it’s making my {Body_part} sweaty.”")
+    print (f"“What are you wearing?” asks {Your_Fiend_First_Name}.")
+    print (f"“My {Item},” you reply. “Why do you want to know?”") 
+    print (f"“I want to rip off your {Item}, {Your_Name}!” says {Your_Fiend_First_Name} lustfully. “I want to rip off your {Item} and grab your sweaty {Body_part}!”")
+    print (f"“But {Your_Fiend_First_Name}, I’m in the middle of {Activity},” you protest.") 
+    print (f"“I don’t care if you’re fucking {Activity}!” cries {Your_Fiend_First_Name}. “I’m horny as hell and I can’t stop thinking about your sweaty {Body_part}! Get your {Body_part} over here!”")
+    print (f"This is an offer too good to refuse. “Sounds good, {Your_Fiend_First_Name}” you reply. “Where are you?”")
+    print (f"“I’m in {Place}, {Your_Name}. Do you want to know what I’m doing in {Place}?”")
+    print ("“Okay…”")
+    print (f"“I’ve got a {Object} and I’m sticking it in my {Orifice}. I’m shoving it as far as it will go.”)
+    print (f"“You’re doing that in {Place}?” you reply incredulously, imagining {Your_Fiend_First_Name}’s {Orifice} stuffed with a {Object}. “I’ll be right there…” you cry in excitement.")
+    print (f"“Wait {Your_Name},” shouts {Your_Fiend_First_Name}, sounding panicked. “I can’t get it out. Every time I try to pull the {Object} out of my {Orifice}, it gets sucked back in again!”")
+    print (f"Have you tried twisting it?” you suggest.") 
+    print (f"“Unngh,” grunts {Your_Fiend_First_Name}. “It’s no good, I can’t get it out!”")
+    print (f"“I’m on my way,” you shout. “Is there anyone there who can help you?”") 
+    print (f"“Wow {Your_Name}, you’re not going to believe this, but {Celebrity} just walked in! Hey {Celebrity}, can you give me a {Body_part} with this {Object}?”")
+    print (f"There’s a lot of grunting and groaning on the other end of the phone as {Celebrity} tries to extract the {Object} from {Your_Fiend_First_Name} {Your_Friend_Last_Name}’s {Orifice}.")
+    print (f"“What’s going on?” you ask anxiously.")
+    print (f"“{Celebrity} is fucking my {Orifice} with the {Object}, {Your_Name}. It’s going in and out, faster and faster. Oh my God, I think I’m going to cum! Oh God, {Your_Name}, I’m cumming!”")
+    print (f"You close your eyes, imagining {Celebrity} pounding {Your_Fiend_First_Name}’s {Orifice} with the {Object} in {Place}. You can’t hold back and your orgasm explodes, saturating your {Item}.")
+    print (f"“Wow {Your_Fiend_First_Name}, that was awesome,” you finally pant into the phone. “I’ll be there soon. Stay right there.”")
+    print (f"“No need, {Your_Name},” replies {Your_Fiend_First_Name}. “{Celebrity} managed to get the {Object} out of my {Orifice}. We’re leaving to find a bigger one. Talk later. Bye.”")
+    print (f"“Oh {Exclamation}!” you exclaim, standing there sadly in your wet {Item}. It’s back to {Activity} for you.)
+else :
+    print ("Incomplete input")
+"""
+# ------------------------------------------------------------------------------------------------------------------------
+
+
+
