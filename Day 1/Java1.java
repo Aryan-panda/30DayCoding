c765e5f2bdeaa2230ef62197f7435d3b1f61ff39
Aryan-panda <aryan06panda@gmail.com>
2024-11-01 09:34:55 +0530

    Update Java1.java

--- Day 1/Java1.java
+++ Day 1/Java1.java
@@ -1,6 +1,29 @@
+//First Code (First Look)
+
+/*
 
 public class Java1 { 
   public static void main (String[] args){
     System.out.println("Aryan is better than others");
   }
 }
+
+*/
+
+//Second Code 
+
+/*
+
+public class Java1 {
+  public static void main (String[] args) {
+    System.out.println("\n");
+    
+    String Name = "Aryan";
+    String Surname = "Panda";
+    System.out.println(Name+" "+Surname);
+
+    System.out.println("\n");
+  }
+}
+
+*/
