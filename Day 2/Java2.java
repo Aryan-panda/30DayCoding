683e05bdcbfde36762d411bd779f69aab1e92321
Aryan-panda <aryan06panda@gmail.com>
2024-11-02 21:31:41 +0530

    Add files via upload

---
+++ Day 2/Java2.java
@@ -0,0 +1,106 @@
+// Warm-up code.
+
+/* 
+
+public class Java2 {
+    public static void main (String[] args) {
+        System.out.println("Hello");
+        char a = 65;
+        float f1 = 6.6f;
+        long l1 = 6000000000000000000l;
+        String Name = "Aryan panda";
+        String THe = "the";
+        System.out.println(l1);
+        System.out.println(Name.length());
+        System.out.println(THe.toUpperCase()+" "+Name.toUpperCase());
+    }
+}
+
+*/
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Math function.
+
+/* 
+
+public class Java2 {
+    public static void main (String[] args) {
+        System.out.println(Math.random());
+        System.out.println((Math.random()*3));
+        System.out.println((int)(Math.random()*3));
+    }
+}
+
+*/
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Very basic if statement. 
+
+/*
+
+public class Java2 {
+    public static void main (String[] args) {
+        final int q = 3;
+        if (q%2==0) {
+            System.out.println(q+" is even.");
+        }
+        else {
+            System.out.println(q+" is odd.");
+        }
+        
+    }
+}
+
+*/
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Array
+
+/*
+
+public class Java2 {
+    public static void main (String[] args) {
+        
+    String[] Name = { "Aryan", "Ayush", "Ritesh", "Aks", "Baba" };
+    
+    for (int i = 0 ; i<Name.length ; i++){
+        System.out.println(Name[i]);
+    }
+    
+    Name[4] = "Ramdev";
+        
+        System.out.println("\n");
+        
+        for (int j = 0 ; j<Name.length ; j++){
+            System.out.println(Name[j]);
+        }
+}
+}
+        
+*/
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Multi-dimentional Array 
+
+/*
+
+public class Java2 {
+    public static void main (String[] args) {
+        
+    int[][] Number = { {1, 2, 3, 4}, {5, 6, 7} };
+    
+    for (int i = 0 ; i < Number.length ; i++){
+        for (int j = 0 ; j < Number[i].length ; j++) {
+            System.out.println(Number[i][j]);
+        }
+}
+}
+}
+
+*/
+
+//-----------------------------------------------------------------------------------------------------------------
